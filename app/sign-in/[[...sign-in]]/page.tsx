799e7c7eb4ac4cc3fb7e580c4e303762222edbc0
najiibmohamed11 <abdinajibmohamed304@example.com>
2025-09-04 16:44:31 +0300

    custom signIn page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,24 @@
-import { SignIn } from "@clerk/nextjs";
 
+'use client'
+
+import * as SignIn from "@clerk/elements/sign-in";
+import * as Clerk from "@clerk/elements/common"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 export default function SignInPagw(){
-    return <SignIn />
+    return (
+      <SignIn.Root>
+        <SignIn.Step name="start">
+            <Clerk.Connection name="google">
+                <div className="flex  min-w-screen justify-center  items-center min-h-screen">
+                   <Clerk.Loading scope="provider:google">{(isLoading)=>{
+                    return isLoading?                    <Button  variant='outline' disabled={isLoading} className="w-2xs cursor-pointer"><Image alt="Google Logo" src='./googl-logo.svg' width={20} height={20}/>Signing in.....</Button>:
+                           <Button  variant='outline' className="w-2xs cursor-pointer"><Image alt="Google Logo" src='./googl-logo.svg' width={20} height={20}/>Continue with Google</Button> }}</Clerk.Loading>
+                    
+                </div>
+            </Clerk.Connection>
+        </SignIn.Step>
+    </SignIn.Root>
+    )
 }
+
