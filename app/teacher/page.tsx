355da3e31cda52bca0066617fc078587b436f39f
najiibmohamed11 <abdinajibmohamed304@gmail.com>
2025-10-08 14:15:55 +0300

    removing un used ssr related imports

--- app/teacher/page.tsx
+++ app/teacher/page.tsx
@@ -3,9 +3,6 @@ import Profile from "../components/Profile";
 import RoomsList from "./components/RoomsList";
 import { ModeToggle } from "../components/ModeToggle";
 import { Rubik } from "next/font/google";
-import { preloadQuery } from "convex/nextjs";
-import { api } from "@/convex/_generated/api";
-import { getToken } from "../hooks/getToken";
 
 const rubik = Rubik({
   subsets: ["latin"],
@@ -13,14 +10,6 @@ const rubik = Rubik({
 });
 
 export default async function Teacher() {
-  const token = await getToken();
-
-  const preloadedTasks = await preloadQuery(
-    api.room.getRooms,
-    {},
-    { token: token },
-  );
-
   return (
     <div className="max-w-6xl px-8 min-h-screen mx-auto ">
       <header className=" h-fit  flex  justify-between mt-4 text-2xl font-bold ">
