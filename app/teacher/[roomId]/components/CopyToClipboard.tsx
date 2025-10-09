d797a599c6fe31764b347a71909693eb673c080b
najiibmohamed11 <abdinajibmohamed304@gmail.com>
2025-10-09 07:36:02 +0300

    fixing copy url issue

--- app/teacher/[roomId]/components/CopyToClipboard.tsx
+++ app/teacher/[roomId]/components/CopyToClipboard.tsx
@@ -2,15 +2,22 @@
 import { Button } from "@/components/ui/button";
 import { Check, Copy } from "lucide-react";
 import { useParams } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function CopyToClipboard() {
   const [isCopied, setIscopied] = useState(false);
+  const [quizeUrl, setQuizeUrl] = useState<string | null>(null);
   const { roomId } = useParams();
-  const quizeUrl = `${window.location.origin}/${roomId}/student`;
+
+  useEffect(() => {
+    setQuizeUrl(`${window.location.origin}/${roomId}/student`);
+  }, [roomId]);
 
   const copyToClipboard = async () => {
     try {
+      if (!quizeUrl) {
+        return;
+      }
       await navigator.clipboard.writeText(quizeUrl);
       setIscopied(true);
       setTimeout(() => {
